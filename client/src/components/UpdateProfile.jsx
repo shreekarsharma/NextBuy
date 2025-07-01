ab37a9cd101613f3d022c251bb9892d7d02ba1bf
shreekarsharma <shreekarsharma@hotmail.com>
2025-07-01 17:07:28 +0530

    update profile fixed

--- client/src/components/UpdateProfile.jsx
+++ client/src/components/UpdateProfile.jsx
@@ -54,7 +54,7 @@ const UpdateProfile = () => {
         form.userName === "" ||
         form.emailAddress === "" ||
         form.phoneNumber === "" ||
-        !form.profilePhoto
+        (!form.profilePhoto && !userData.profilePhoto)
       ) {
         alert("Please fill the details");
         return;
